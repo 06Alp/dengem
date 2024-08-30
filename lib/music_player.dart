967a0eb9d3abf23cee1e6727454ce77a7083fcfd
Tarik Alp Dincsoy <141755202+06Alp@users.noreply.github.com>
2024-08-31 00:17:38 +0300

    Add files via upload

--- lib/music_player.dart
+++ lib/music_player.dart
@@ -1,32 +1,34 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:dengem/playlist_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dengem/playlist_provider.dart';
 
 class MusicPlayer extends StatefulWidget {
+  const MusicPlayer({super.key});
+
   @override
-  _MusicPlayerState createState() => _MusicPlayerState();
+  _MusicPlayer createState() => _MusicPlayer();
 }
 
-class _MusicPlayerState extends State<MusicPlayer> {
+class _MusicPlayer extends State<MusicPlayer> {
   final AudioPlayer audioPlayer = AudioPlayer();
   bool isPlaying = false;
   double progress = 0.0;
   bool isShuffling = false;
   bool isRepeating = false;
   Duration duration = Duration.zero;
+  int musicIndex = 1;
 
   @override
   void initState() {
     super.initState();
 
-    // Listen to audio duration changes
     audioPlayer.onDurationChanged.listen((Duration newDuration) {
       setState(() {
         duration = newDuration;
       });
     });
 
-    // Listen to audio position changes
     audioPlayer.onPositionChanged.listen((Duration newPosition) {
       setState(() {
         progress = newPosition.inMilliseconds.toDouble();
@@ -34,13 +36,22 @@ class _MusicPlayerState extends State<MusicPlayer> {
     });
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 218, 157, 74),
+        backgroundColor: const Color.fromARGB(255, 218, 157, 74),
         centerTitle: true,
         title: const Text('Klasik Müzik'),
+        actions: [
+          IconButton(
+              onPressed: () {} /*send to home page*/,
+              icon: const Icon(
+                Icons.close_rounded,
+                size: 25,
+              ))
+        ],
       ),
       drawer: Drawer(
         backgroundColor: Colors.white,
@@ -58,13 +69,13 @@ class _MusicPlayerState extends State<MusicPlayer> {
               padding: const EdgeInsets.only(left: 5),
               child: ListTile(
                 title: const Text('DOĞA SESİ'),
-                leading: Icon(Icons.nature_outlined),
+                leading: const Icon(Icons.nature_outlined),
                 onTap: () {
                   Navigator.pop(context);
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => MusicPlayer(),
+                        builder: (context) => const MusicPlayer(),
                       ));
                 },
               ),
@@ -73,13 +84,13 @@ class _MusicPlayerState extends State<MusicPlayer> {
               padding: const EdgeInsets.only(left: 5),
               child: ListTile(
                 title: const Text('KLASİK MÜZİK'),
-                leading: Icon(Icons.music_note),
+                leading: const Icon(Icons.music_note),
                 onTap: () {
                   Navigator.pop(context);
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => MusicPlayer(),
+                        builder: (context) => const MusicPlayer(),
                       ));
                 },
               ),
@@ -89,7 +100,8 @@ class _MusicPlayerState extends State<MusicPlayer> {
       ),
       body: Column(
         children: [
-          Expanded(flex: 2,
+          Expanded(
+            flex: 2,
             child: Container(
               decoration: const BoxDecoration(
                   borderRadius: BorderRadius.only(
@@ -98,25 +110,30 @@ class _MusicPlayerState extends State<MusicPlayer> {
                   color: Color.fromARGB(255, 218, 157, 74)),
               child: Align(
                 alignment: Alignment.bottomCenter,
-                child: Image.asset(
-                  playlist[0].albumeImagePath,
-                  height: 250,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    playlist[musicIndex].albumeImagePath,
+                    height: 250,
+                  ),
                 ),
               ),
             ),
           ),
-          Expanded(flex: 2,
+          Expanded(
+            flex: 2,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
-                    playlist[0].songName,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    playlist[musicIndex].songName,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Text(playlist[0].artistName),
+                Text(playlist[musicIndex].artistName),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Row(
@@ -132,20 +149,33 @@ class _MusicPlayerState extends State<MusicPlayer> {
                         },
                       ),
                       IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            if (musicIndex == 0) {
+                              musicIndex = playlist.length - 1;
+                            } else {
+                              musicIndex--;
+                            }
+                            audioPlayer.play(
+                                AssetSource(playlist[musicIndex].audioPath));
+                          });
+                        },
                         icon: const Icon(Icons.skip_previous_rounded),
                         iconSize: 30,
                       ),
                       IconButton(
                         icon: Icon(
-                          isPlaying ? Icons.pause_rounded : Icons.play_arrow_rounded,
+                          isPlaying
+                              ? Icons.pause_rounded
+                              : Icons.play_arrow_rounded,
                           size: 50,
                         ),
                         onPressed: () async {
                           if (isPlaying) {
                             await audioPlayer.pause();
                           } else {
-                            await audioPlayer.play(AssetSource(playlist[0].audioPath));
+                            await audioPlayer.play(
+                                AssetSource(playlist[musicIndex].audioPath));
                           }
                           setState(() {
                             isPlaying = !isPlaying;
@@ -153,13 +183,23 @@ class _MusicPlayerState extends State<MusicPlayer> {
                         },
                       ),
                       IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            if (musicIndex == playlist.length - 1) {
+                              musicIndex = 0;
+                            } else {
+                              musicIndex++;
+                            }
+                            audioPlayer.play(
+                                AssetSource(playlist[musicIndex].audioPath));
+                          });
+                        },
                         icon: const Icon(Icons.skip_next_rounded),
                         iconSize: 30,
                       ),
                       IconButton(
-                        icon:
-                            Icon(Icons.repeat, color: isRepeating ? Colors.blue : null),
+                        icon: Icon(Icons.repeat,
+                            color: isRepeating ? Colors.blue : null),
                         onPressed: () {
                           setState(() {
                             isRepeating = !isRepeating;
@@ -176,7 +216,7 @@ class _MusicPlayerState extends State<MusicPlayer> {
                     onChanged: (value) {
                       audioPlayer.seek(Duration(milliseconds: value.toInt()));
                     },
-                    activeColor: Color.fromARGB(255, 218, 157, 74),
+                    activeColor: const Color.fromARGB(255, 218, 157, 74),
                     min: 0.0,
                     max: duration.inMilliseconds.toDouble(),
                   ),
